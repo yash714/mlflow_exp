1a562bcfd69e3c22ce0a2ccf636e4efd8040e892
yash714 <yashsharma09147@gmail.com>
2023-11-26 20:43:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,85 @@
+import os
+import warnings
+import sys
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from urllib.parse import urlparse
+import mlflow
+from mlflow.models import infer_signature
+import mlflow.sklearn
+
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
+
+    # Replace 'wine-quality.csv' with the actual path to your downloaded CSV file
+    csv_path = "wine-quality.csv"
+
+    try:
+        data = pd.read_csv(csv_path, sep=",")
+    except Exception as e:
+        logger.exception("Error loading the CSV file. Please check the file path. Error: %s", e)
+        sys.exit(1)
+
+    # Print the column names to identify the correct column names
+    print("Column names:", data.columns)
+
+    # Replace "feature_column_names" and "target_column_name" with the correct names
+    feature_column_names = ["feature1", "feature2", ..., "last_feature"]
+    target_column_name = "quality"
+
+    # Adjust the column names in the next lines based on the printed names
+    train, test = train_test_split(data)
+    train_x = train.drop([target_column_name], axis=1)
+    test_x = test.drop([target_column_name], axis=1)
+    train_y = train[[target_column_name]]
+    test_y = test[[target_column_name]]
+
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        predictions = lr.predict(train_x)
+        signature = infer_signature(train_x, predictions)
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                lr, "model", registered_model_name="ElasticnetWineModel"
+            )
+        else:
+            mlflow.sklearn.log_model(lr, "model")
